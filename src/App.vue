db025b4be9cb41c24b194ab12748931256cb5e3a
Tonys <info@tony-schiller.com>
2023-07-06 18:49:23 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <aside>
+  </aside>
+  <header>
+    <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div >
+      "You did it!"
+
+    </div>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
