6efba44971c0b7fdf44afd36b219f2ee61cfcd18
Tonys <info@tony-schiller.com>
2023-07-06 19:32:22 +0200

    inital impressions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+
 
 </script>
 
 <template>
-  <aside>
-  </aside>
   <header>
-    <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div >
+    <div>
       "You did it!"
 
     </div>
