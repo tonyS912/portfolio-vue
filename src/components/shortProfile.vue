60d5730cadbdd5c272e7d18c0b7a0e0d6774acc7
Tonys <info@tony-schiller.com>
2023-07-09 01:18:20 +0200

    update first view

---
+++ src/components/shortProfile.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+
+</script>
+
+<template>
+    
+    <div id="profile">
+        <div>
+            <div class="d-flex justify-content-center align-self-center my-4 py-4">
+                <img class="w-50 rounded-circle bg-warning" src="../assets/img/tony-profile.png" alt="profile image">
+            </div>
+        </div>
+        <div>
+            <div class="d-flex justify-content-center align-self-center pb-4"><label class="text-warning fs-5">Tony Schiller</label></div>
+        </div>
+        <div>
+            <div class="d-flex justify-content-center align-self-center pb-4"><label class="text-warning text-uppercase text-decoration-underline fs-5">Fullstack Developer</label></div>
+        </div>
+        <div class="d-flex flex-row justify-content-evenly my-2">
+            <div>
+                <a href="https://www.linkedin.com/in/tony-schiller-0995aa202" target="_blank">
+                    <img style="height: 34px;" src="../assets/img/Linkedin.svg" alt="LinkedIn Image">
+                </a>
+            </div>
+            <div>
+                <a href="https://www.youtube.com/" target="_blank">
+                    <img style="height: 34px;" src="../assets/img/Youtube.svg" alt="Youtube Image">
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/tonyS912" target="_blank">
+                    <img style="height: 34px;" src="../assets/img/Github.svg" alt="Github Image">
+                </a>
+            </div>
+        </div>
+
+        <div class="mx-4 my-5 bg-danger" style="height: 3px"></div>
+    </div>
+
+
+    <div id="about">
+
+    </div>
+
+    <div id="language">
+
+    </div>
+
+    <div id="skills">
+
+    </div>
+
+    <div id="extraSkills">
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
