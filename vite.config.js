9dc279de026805d4d340f3818c0336ebaa3eff8c
Tonys <info@tony-schiller.com>
2023-07-31 21:16:26 +0200

    vite update deployment updates

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        '/src/assets/img/join.png',
+        '/src/assets/img/github-btn.png',
+        '/src/assets/img/logo_tony.png',
+        '/src/assets/img/tony-profile.png',
+      ]
+    }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/dist' : '/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        'src/assets/img/join.png',
-        'src/assets/img/github-btn.png',
-        'src/assets/img/logo_tony.png',
-      ]
     }
   }
 })
